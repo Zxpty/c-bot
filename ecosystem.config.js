f12d958ffa0a6256ff3fb579d6e2c67e284887a6
Anthony <anthony-08.2004@hotmail.com>
2026-01-10 12:12:36 -0500

    update

---
+++ ecosystem.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  apps: [{
+    name: 'c-bot',
+    script: 'main.py',
+    interpreter: '/home/anthony/Desktop/Projects/c-bot/venv/bin/python',
+    cwd: '/home/anthony/Desktop/Projects/c-bot',
+    env: {
+        
+    }
+  }]
+};
